9ade81b1c200b42d6382670731c78a2de29af704
Alex Asichka <sanechka3@mail.ru>
2023-02-15 19:50:05 +0200

    change light and dark settings

--- Clima_Project/WeatherViewController.swift
+++ Clima_Project/WeatherViewController.swift
@@ -18,18 +18,8 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        temperatureLabel.text = "33"
+        cityLabel.text = "Vilnius"
+        conditionalImageView.image = UIImage(systemName: "sun.min")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
