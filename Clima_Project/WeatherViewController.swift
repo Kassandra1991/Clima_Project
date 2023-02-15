b8e786908c2698bcfa89acb407bb8076722d0b62
Alex Asichka <sanechka3@mail.ru>
2023-02-15 13:51:34 +0200

    first settings

---
+++ Clima_Project/WeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherViewController.swift
+//  Clima_Project
+//
+//  Created by Aleksandra Asichka on 2023-02-15.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
